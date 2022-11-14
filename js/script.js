2777139febcc0da30918b632a61d28895bf0477e
Marcin <marcin@dobrzanski.me>
2022-11-14 22:59:38 +0100

    Fix problem with block and for of loop

--- js/script.js
+++ js/script.js
@@ -1,61 +1,63 @@
-const titleClickHandler = function (event) {
-	event.preventDefault();
-	console.log('Link was clicked!');
-	console.log(event);
-
-	/* [DONE] remove class 'active' from all article links  */
-
-	const activeLinks = document.querySelectorAll('.titles a.active');
-
-	for (let activeLink of activeLinks) {
-		activeLink.classList.remove('active');
-	}
-
-	/* [DONE] add class 'active' to the clicked link */
-	const clickedElement = this;
-	{
-		clickedElement.classList.add('active');
-	}
-	console.log('clickedElement:', clickedElement);
-
-	/* [DONE] remove class 'active' from all articles */
-
-	const activeArticles = document.querySelectorAll('.posts article.active');
-
-	for (let activeArticle of activeArticles) {
-		activeArticle.classList.remove('active');
-	}
-
-	/* [DONE] get 'href' attribute from the clicked link */
-
-	const articleSelector = clickedElement.getAttribute('href');
-	console.log(articleSelector);
-
-	/* [DONE] find the correct article using the selector (value of 'href' attribute) */
-
-	const targetArticle = document.querySelector(articleSelector);
-
-	/* add class 'active' to the correct article */
-
-	{
-		targetArticle.classList.add('active');
-	}
-	console.log(targetArticle);
-};
-
-const links = document.querySelectorAll('.titles a');
-
-for (let link of links) {
-	link.addEventListener('click', titleClickHandler);
-}
 {
+	const titleClickHandler = function (event) {
+		event.preventDefault();
+		console.log('Link was clicked!');
+		console.log(event);
+
+		/* [DONE] remove class 'active' from all article links  */
+
+		const activeLinks = document.querySelectorAll('.titles a.active');
+
+		for (let activeLink of activeLinks) {
+			activeLink.classList.remove('active');
+		}
+
+		/* [DONE] add class 'active' to the clicked link */
+		const clickedElement = this;
+		{
+			clickedElement.classList.add('active');
+		}
+		console.log('clickedElement:', clickedElement);
+
+		/* [DONE] remove class 'active' from all articles */
+
+		const activeArticles = document.querySelectorAll('.posts article.active');
+
+		for (let activeArticle of activeArticles) {
+			activeArticle.classList.remove('active');
+		}
+
+		/* [DONE] get 'href' attribute from the clicked link */
+
+		const articleSelector = clickedElement.getAttribute('href');
+		console.log(articleSelector);
+
+		/* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+		const targetArticle = document.querySelector(articleSelector);
+
+		/* add class 'active' to the correct article */
+
+		targetArticle.classList.add('active');
+
+		console.log(targetArticle);
+	};
+
+	const links = document.querySelectorAll('.titles a');
+
+	for (let link of links) {
+		link.addEventListener('click', titleClickHandler);
+	}
+
 	const optArticleSelector = '.post',
 		optTitleSelector = '.post-title',
 		optTitleListSelector = '.titles';
 
 	function generateTitleLinks() {
 		/* remove contents of titleList */
-		const titleList = (document.querySelectorAll(optTitleListSelector).innerHTML = '');
+		const titleList = document.querySelector(optTitleListSelector);
+		console.log(titleList);
+		titleList.innerHTML = '';
 
 		/* for each article */
 		const articles = document.querySelectorAll(optArticleSelector);
@@ -63,14 +65,15 @@ for (let link of links) {
 
 		for (let article of articles) {
 			console.log(article);
+
+			/* get the article id */
+			const articleId = article.getAttribute('id');
+			console.log(articleId);
+			/* find the title element */
+			/* get the title from the title element */
+			/* create HTML of the link */
+			/* insert link into titleList */
 		}
-		/* get the article id */
-		const articleId = optArticleSelector.getAttribute('id');
-		console.log(articleId);
-		/* find the title element */
-		/* get the title from the title element */
-		/* create HTML of the link */
-		/* insert link into titleList */
 	}
 
 	generateTitleLinks();
